cc5dc3222cdf65b2866ee2f21527ac71ffc11de7
mike yang <18604713262@163.com>
2022-06-14 19:24:27 -0400

    ios support

---
+++ build_ios.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+ng build --localize
+rm -rf ios/App/App/public/en-US
+mkdir ios/App/App/public/en-US
+find ios/App/App/public/ -name "main*.*" -exec mv '{}' ios/App/App/public/en-US \;
+find ios/App/App/public/ -name "polyfills*.*" -exec mv '{}' ios/App/App/public/en-US \;
+find ios/App/App/public/ -name "styles*.*" -exec mv '{}' ios/App/App/public/en-US \;
+find ios/App/App/public/ -name "runtime*.*" -exec mv '{}' ios/App/App/public/en-US \;
