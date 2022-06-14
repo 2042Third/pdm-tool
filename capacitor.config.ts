c841463d04c54e679bd1c0bc7cec119d77ce0580
Mike Yang <18604713262@163.com>
2022-06-14 19:41:53 -0400

    ios setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pdm.pw.pdm',
+  appName: 'pdm-notes',
+  webDir: 'dist/pdm-notes',
+  bundledWebRuntime: false
+};
+
+export default config;
