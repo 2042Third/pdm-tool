93898bc8e1cf89cff8494ef4aa9e520d8cf82af4
mike yang <18604713262@163.com>
2022-07-18 16:47:47 -0400

    App password toggle and time out length settings, info updates

--- src/app/platform/settings-dialog/settings-dialog.component.ts
+++ src/app/platform/settings-dialog/settings-dialog.component.ts
@@ -7,14 +7,42 @@ import {UserinfoService} from "../../_services/userinfos.service";
   styleUrls: ['./settings-dialog.component.scss'],
 })
 export class SettingsDialogComponent implements OnInit {
-  panelOpenState = false;
+  GeneralPanelOpenState = false;
+  SecurityPanelOpenState = false;
+  // General
+  // Security
+  security_list_timeout = [
+    {timeout:5, name:"5"},
+    {timeout:20, name:"20"},
+    {timeout:60, name:"60"},
+    {timeout:720, name:"720"},
+  ];
+  timeout_value: number;
+  app_pw_toggle: boolean = true;
 
   constructor(
     public userinfo: UserinfoService
   ) {
-
+    this.timeout_value = userinfo.cookies_timeout;
   }
 
+
+  security_change_save_timeout(e): void{
+    let name = e.target.value;
+    let list = this.security_list_timeout.filter(x => x.name === name)[0];
+    console.log(`Change app pw timeout to = ${this.timeout_value}`);
+    if (this.timeout_value == 0){
+      this.timeout_value = 20;
+    }
+    this.userinfo.cookies_timeout = this.timeout_value;
+  }
+  clear_timeouts(){
+    this.timeout_value = 0;
+  }
   ngOnInit() {}
 
+  security_toggle_app_pw(e) {
+    console.log(`Change app pw toggle to = ${this.app_pw_toggle}`);
+
+  }
 }
